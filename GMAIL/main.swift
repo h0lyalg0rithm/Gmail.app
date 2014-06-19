c485ae93c66f401808a2fee452324a11de28a155
Suraj Shirvankar <surajshirvankar@gmail.com>
2014-06-19 11:35:43 +0400

    Initial Commit

---
+++ GMAIL/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  GMAIL
+//
+//  Created by Suraj Shirvankar on 6/19/14.
+//  Copyright (c) 2014 com.crazydevs. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
